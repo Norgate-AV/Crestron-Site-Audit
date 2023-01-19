8a4b5a2f8441e345eb523da53295f4b44916146c
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2023-01-19 17:53:29 +0000

    refactor: rename function, improve functionality

---
+++ lib/utils/Read-ToolboxAddressBook.ps1
@@ -0,0 +1,182 @@
+<#
+ _   _                       _          ___     __
+| \ | | ___  _ __ __ _  __ _| |_ ___   / \ \   / /
+|  \| |/ _ \| '__/ _` |/ _` | __/ _ \ / _ \ \ / /
+| |\  | (_) | | | (_| | (_| | ||  __// ___ \ V /
+|_| \_|\___/|_|  \__, |\__,_|\__\___/_/   \_\_/
+                 |___/
+
+MIT License
+
+Copyright (c) 2022 Norgate AV Solutions Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+function Get-ToolboxAddressBookDataList {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AddressBookData,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [regex]
+        $Pattern
+    )
+
+    begin {
+        $list = ""
+    }
+
+    process {
+        $patternMatch = $Pattern.Match($AddressBookData)
+
+        if (!$patternMatch.Success) {
+            throw "Data list matching pattern not found in address book: $Pattern"
+        }
+
+        $list = $patternMatch.Groups["list"].Value.Trim()
+    }
+
+    end {
+        $list
+    }
+}
+
+function Convert-ToolboxAddressBookDeviceList {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $DeviceList
+    )
+
+    $devices = @()
+
+    $pattern = [regex] '(?:(?<name>[\w\d\. _-]+)=(?<connection>[\w]+) (?<address>[\w\d\. _-]+)(?:,(?<port>\d+))?(?:;\w+ (?<username>[\w\d]+))?(?:;\w+ (?<password>[\w\d!@£$%^&*#_-]+))?(?:;console (?<console>\w+))?(?:;\w+ (?<pat_connection>\w+),)?(?:(?<pat_hostname>[\w\d\. _-]+),?)?(?:(?<pat_port>\d+))?)'
+
+    $patternMatches = $pattern.Matches($deviceList)
+
+    if ($patternMatches.Count -eq 0) {
+        throw "No devices found in address book."
+    }
+
+    $patternMatches | ForEach-Object {
+        $match = $_
+
+        $devices += [PSCustomObject] @{
+            Name          = $match.Groups["name"].Value.Trim()
+            Connection    = $match.Groups["connection"].Value.Trim()
+            Address       = $match.Groups["address"].Value.Trim()
+            Port          = $match.Groups["port"].Value.Trim()
+            Username      = $match.Groups["username"].Value.Trim()
+            Password      = $match.Groups["password"].Value.Trim()
+            Console       = $match.Groups["console"].Value.Trim()
+            PATConnection = $match.Groups["pat_connection"].Value.Trim()
+            PATHostname   = $match.Groups["pat_hostname"].Value.Trim()
+            PATPort       = $match.Groups["pat_port"].Value.Trim()
+        }
+    }
+
+    return $devices
+}
+
+function Convert-ToolboxAddressBookCommentList {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject[]]
+        $Devices,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $CommentList
+    )
+
+    $pattern = [regex] '(?:(?<name>[\w\d\. _-]+)=(?<comment>.+)?)'
+
+    $patternMatches = $pattern.Matches($CommentList)
+
+    if ($patternMatches.Count -eq 0) {
+        throw "No comments found in address book."
+    }
+
+    $patternMatches | ForEach-Object {
+        $match = $_
+
+        $device = $Devices | Where-Object { $_.Name -eq $match.Groups["name"].Value.Trim() }
+
+        if ($device) {
+            $device | Add-Member Comment $match.Groups["comment"].Value.Trim()
+        }
+    }
+
+    return $Devices
+}
+
+function Read-ToolboxAddressBook {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AddressBook
+    )
+
+    $devices = @()
+
+    try {
+        $AddressBook = Resolve-Path -Path $AddressBook
+
+        try {
+            $content = Get-Content -Path $AddressBook -Raw
+        }
+        catch {
+            throw "Unable to read address book file: $AddressBook"
+        }
+
+        $deviceList = $content | Get-ToolboxAddressBookDataList -Pattern '\[ComSpecs\](?<list>[\s\S]+)\[Notes\]'
+
+        $devices += Convert-ToolboxAddressBookDeviceList -DeviceList $deviceList
+
+        $commentList = $content | Get-ToolboxAddressBookDataList -Pattern '\[Notes\](?<list>[\s\S]+)(?:\[ExtComSpecs\])?'
+
+        $devices = Convert-ToolboxAddressBookCommentList -Devices $devices -CommentList $commentList
+    }
+    catch {
+        Write-Error "$($_.Exception.GetBaseException().Message)"
+    }
+    finally {
+        $devices
+    }
+}
+
+if ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path) {
+    Read-ToolboxAddressBook @args
+}
